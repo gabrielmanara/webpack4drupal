aeacd2838d2beee6f7d7ffd80e631b0a36d3b751
Gabriel Manara <gmanara@its.jnj.com>
2018-04-06 12:12:28 -0300

    Config page twig

--- src/vue/components/Message.vue
+++ src/vue/components/Message.vue
@@ -1,3 +1,8 @@
 <template>
-    <h1>Hello webpack4drupal</h1>
+    <div>
+        <h1>Hello webpack4drupal - This is a Vue Component</h1>
+        <div>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Vue.js_Logo.svg/400px-Vue.js_Logo.svg.png" alt="">
+        </div>
+    </div>
 </template>
