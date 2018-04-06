2cea663cf5416db64801bc957bd20d60c3d78e18
Gabriel Manara <gmanara@its.jnj.com>
2018-04-06 12:05:30 -0300

    Babel plugin for async await

--- src/index.js
+++ src/index.js
@@ -26,9 +26,20 @@ console.log(sum(...numbers)); // expected output: 6
 
 // Async Operator
 // Ex: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
-async function callback(x) {
-    let a = await callback1(x);
-    let b = await callback2(a);
-    return b;
+function resolveAfter2Seconds(x) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+        resolve(x);
+    }, 2000);
+});
 }
-callback(10);
+
+async function add1(x) {
+    const a = await resolveAfter2Seconds(20);
+    const b = await resolveAfter2Seconds(30);
+    return x + a + b;
+}
+
+add1(10).then(v => {
+    console.log(v);  // prints 60 after 4 seconds.
+});
